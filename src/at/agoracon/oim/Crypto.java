616a8dd5f9326624c14f910fb1dc61be9c00722b
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2016-05-03 16:40:07 +0200

    encrypting added

--- src/at/agoracon/oim/Crypto.java
+++ src/at/agoracon/oim/Crypto.java
@@ -8,24 +8,95 @@ import com.thortech.xl.crypto.tcCryptoUtil;
 
 public class Crypto
 {
+    private static String key = "DBSecretKey";
+    private static String sep = ":";
+
+    // public ---------------------------------------------------------
+    //
     public static void main(String[] args) 
-	throws tcCryptoException, IOException, ParseException
+    throws tcCryptoException, IOException, ParseException
     {
-	String l, sep = ":";
+        Boolean modeEncrypt = false,
+                modeMarker = false,
+                modeHelp = false;
+        String  line,
+                input,
+                result;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-	System.out.println("Decrypting stdin, stop with Ctrl-Z...");
+        // set run modes
+        //
+        for (int i = 0; i < args.length; i++)
+        {
+            switch (args[i])
+            {
+                case "-e":  modeEncrypt = true;
+                            break;
+                case "-d":  modeEncrypt = false;
+                            break;
+                case "-m":  modeMarker = true;
+                            break;
+                case "-h":  modeHelp = true;
+                            break;
+            }
+        }
+        if (modeHelp == true)
+        {
+            printHelp();
+            return;
+        }
 
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.err.printf("%s stdin, stop with EOF or Ctrl-Z...\n",
+                (modeEncrypt ? "Encrypting" : "Decrypting"));
 
-	while ((l = in.readLine()) != null && l.length() != 0) {
-	    if (l.contains(sep)) {
-		String[] parts = l.split(sep, 2);
-		System.out.println(parts[0] + ":" + tcCryptoUtil.decrypt(parts[1], 
-			    "DBSecretKey"));
-	    } else {
-		throw new ParseException("String " + l + " does not contain " + sep, l.length());
-	    }
-	}
+        // main loop
+        //
+        while ((line = in.readLine()) != null && line.length() != 0)
+        {
+            if (modeMarker)
+            {
+                String[] parts = line.split(sep, 2);
+                result = parts[0] + sep;
+                input  = parts[1];
+            }
+            else
+            {
+                result = "";
+                input  = line;
+            }
+
+            result += ( modeEncrypt ? encrypt(input) : decrypt(input) );
+
+            System.out.println(result);
+        }
+    }
+
+    // private ------------------------------------------------------
+    //
+    private static void printHelp()
+    {
+        System.out.println(
+                "Encryption/decryption of STDIN using the OIM instance key"
+              + "In marker mode you can prepend each record with a marker"
+              + "that will be present in output row:"
+              + "<marker>:<data>"
+              + ""
+              + "Options:"
+              + "    -d     decrypt"
+              + "    -e     encrypt"
+              + "    -m     marker mode");
+    }
+
+    private static String decrypt(String enc)
+    throws tcCryptoException
+    {
+        return tcCryptoUtil.decrypt(enc, key);
+    }
+
+    private static String encrypt(String plain)
+    throws tcCryptoException
+    {
+        return tcCryptoUtil.encrypt(plain, key);
     }
 
 }
